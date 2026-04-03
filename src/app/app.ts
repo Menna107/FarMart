4d0a17706fb82cb964b2ae0ba48c055e577b7518
Menna <menna0hekal2014@gmail.com>
2026-04-03 10:57:59 +0200

    edit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { ToastComponent } from './shared/ui/toast/toast.component';
@@ -11,12 +11,11 @@ import { NgxSpinnerComponent } from 'ngx-spinner';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App implements OnInit {
+export class App {
   protected readonly title = signal('eCommerce');
-  protected readonly router = inject(Router);
   ngOnInit() {
-    if (window.location.pathname === '/allorders') {
-      this.router.navigate(['/allorders']);
+    if (window.location.pathname === '/allorders' && !window.location.hash) {
+      window.location.replace('/#/allorders');
     }
   }
 }
