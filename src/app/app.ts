8a05630770aab9f9cdc9d383ff5f7692204a6c10
Menna <menna0hekal2014@gmail.com>
2026-04-08 22:14:17 +0200

    edit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, signal, PLATFORM_ID, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { ToastComponent } from './shared/ui/toast/toast.component';
 import { NgxSpinnerComponent } from 'ngx-spinner';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,18 @@ import { NgxSpinnerComponent } from 'ngx-spinner';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('eCommerce');
+  private readonly platformId = inject(PLATFORM_ID);
+  private readonly router = inject(Router);
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const path = window.location.pathname;
+
+      if (path === '/allorders') {
+        this.router.navigate(['/allorders']);
+      }
+    }
+  }
 }
